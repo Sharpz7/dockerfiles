8f138437285ba2adf94705d3ec1e7e66b721db73
unknown <adam.mcarthur62@gmail.com>
2019-04-22 14:03:35 +0100

    Changed conn

--- hastebin/rethink.js
+++ hastebin/rethink.js
@@ -1,4 +1,8 @@
-var r = require('rethinkdbdash')();
+var r = require('rethinkdbdash')({
+    servers: [
+        {host: 'rdb', port: 28015},
+    ]
+});
 
 r.dbCreate('haste').run();
 r.db('heroes').tableCreate('uploads').run();
