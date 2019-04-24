987baf72f416c585a56166a7e31d9cd596290a29
unknown <adam.mcarthur62@gmail.com>
2019-04-24 14:18:28 +0100

    Moving to circleci.

---
+++ .circleci/build.sh
@@ -0,0 +1,9 @@
+arr=(./*)
+
+for ((i=0; i<${#arr[@]}; i++)); do
+    if (${arr[$i]} -ne ".circleci") {
+        docker pull sharp6292/${arr[$i]}:latest || true
+        docker build --cache-from sharp6292/${arr[$i]}:latest -f "${arr[$i]}/Dockerfile" -t sharp6292/${arr[$i]}:latest .
+        docker push sharp6292/${arr[$i]}:latest
+    }
+done
