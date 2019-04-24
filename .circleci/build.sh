6c855099d25b53e62d2d185314465bf595e1e2db
unknown <adam.mcarthur62@gmail.com>
2019-04-24 14:48:28 +0100

    Tried to get step to trigger.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-arr=$(ls ./)
+arr=$(ls -d ./)
 
 for item in $arr; do
-    if ($item -ne ".circleci"); then
+    if ("$item" -ne ".circleci"); then
+        cd "$item"
         docker pull sharp6292/${item}:latest || true
-        docker build --cache-from sharp6292/${item}:latest -f "${item}/Dockerfile" -t sharp6292/${item}:latest .
+        docker build --cache-from sharp6292/${item}:latest -f "Dockerfile" -t sharp6292/${item}:latest .
         docker push sharp6292/${item}:latest
+        cd ..
     fi
 done
