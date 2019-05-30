8cb995913460e13e121ee49081b249441c3785de
Sharpz7 <adam.mcarthur62@gmail.com>
2019-05-30 20:09:14 +0100

    Testing python builds

---
+++ build.py
@@ -0,0 +1,37 @@
+import os
+
+BANNED = [".circleci", ".git", "build.py", "README.md", "builds", ".gitignore"]
+
+locs = os.listdir(".")
+builds = []
+for loc in locs:
+    if loc not in BANNED:
+        builds.append(loc)
+
+num = len(builds) // 4
+
+total = {"1": num, "2": num, "3": num, "4": num}
+if len(builds) % 4 != 0:
+    for i in range(1, (len(builds) % 4)+1):
+        total[str(i)] += 1
+
+count = 0
+job = 1
+job_code = ""
+for build in builds:
+    count += 1
+    sh = (
+        f"docker login -u $DOCKER_USER -p $DOCKER_PASS\n"
+        f"cd {build}\n"
+        f"docker pull sharp6292/{build}:latest || true\n"
+        f"docker build --cache-from sharp6292/{build}:latest "
+        f"-f Dockerfile -t sharp6292/{build}:latest .\n"
+        f"docker push sharp6292/{build}:latest\n"
+        "cd ..\n"
+    )
+    job_code += sh
+    if count >= total[str(job)]:
+        count = 0
+        with open(f'./builds/build-{job}.sh', 'w') as file:
+            file.write(job_code)
+        job += 1
