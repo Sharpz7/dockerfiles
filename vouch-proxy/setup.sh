245658f3529946b45d15574b8935536ad6f959b4
Sharpz7 <adam.mcarthur62@gmail.com>
2019-08-09 14:48:59 +0100

    Bugfixes.

--- vouch-proxy/setup.sh
+++ vouch-proxy/setup.sh
@@ -1,4 +1,4 @@
-sed -i -e "s/XXXXX1/${VOUCHID}/g" /config.yml
-sed -i -e "s/XXXXX2/${VOUCHSECRET}/g" /config.yml
+sed -i -e "s/XXXXX1/${VOUCHID}/g" /config/config.yml
+sed -i -e "s/XXXXX2/${VOUCHSECRET}/g" /config/config.yml
 
 echo "Successfully edited config file."
