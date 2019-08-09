791b663ea9656fff5ae714d0e130214e73088bc2
Sharpz7 <adam.mcarthur62@gmail.com>
2019-08-09 15:04:06 +0100

    Bugfix: make vouch proxy loc relative.

--- vouch-proxy/setup.sh
+++ vouch-proxy/setup.sh
@@ -1,7 +1,5 @@
 sed -i -e "s/XXXXX1/${VOUCHID}/g" /config/config.yml
 sed -i -e "s/XXXXX2/${VOUCHSECRET}/g" /config/config.yml
 
-ls -a
-
 echo "Successfully edited config file."
-/vouch-proxy
+./vouch-proxy
