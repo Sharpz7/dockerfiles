9139006113de5ef86d6b529786026475f8482714
Sharpz7 <adam.mcarthur62@gmail.com>
2019-08-17 16:37:33 +0100

    More efficient build.py file.
    3 new images for ci.

--- build.py
+++ build.py
@@ -1,38 +1,49 @@
 import os
 
 BANNED = [".circleci", ".git", "build.py", "README.md", "builds", ".gitignore"]
+EXECUTOR_COUNT = 4
+
+
+def split_builds(builds, n):
+    return [builds[i::n] for i in range(n)]
+
+
+class Build:
+    def __init__(self, name):
+        self.name = name
+        self.version = self.check_version()
+
+    def check_version(self):
+        if os.path.isfile(f"./{self.name}/.version"):
+            with open(f"./{self.name}/.version", 'r') as file:
+                return file.read()
+
+        else:
+            return "latest"
+
+    def __repr__(self):
+        return f"{self.name}, {self.version}"
+
 
 locs = os.listdir(".")
 builds = []
 for loc in locs:
     if loc not in BANNED:
-        builds.append(loc)
+        builds.append(Build(loc))
+split = split_builds(builds, EXECUTOR_COUNT)
 
-num = len(builds) // 4
-
-total = {"0": num, "1": num, "2": num, "3": num}
-if len(builds) % 4 != 0:
-    for i in range(1, (len(builds) % 4)+1):
-        total[str(i)] += 1
-
-count = 0
-job = 0
-job_code = "cd ..\n"
-for build in builds:
-    count += 1
-    sh = (
-        f"docker login -u $DOCKER_USER -p $DOCKER_PASS\n"
-        f"cd {build}\n"
-        f"docker pull sharp6292/{build}:latest || true\n"
-        f"docker build --cache-from sharp6292/{build}:latest "
-        f"-f Dockerfile -t sharp6292/{build}:latest .\n"
-        f"docker push sharp6292/{build}:latest\n"
-        "cd ..\n"
-    )
-    job_code += sh
-    if count >= total[str(job)]:
-        with open(f'./builds/build-{job}.sh', 'w') as file:
-            file.write(job_code)
-        job += 1
-        job_code = "cd ..\n"
-        count = 0
+for num, executor in enumerate(split):
+    job_code = ""
+    for build in executor:
+        sh = (
+            f"docker login -u $DOCKER_USER -p $DOCKER_PASS\n"
+            f"cd {build.name}\n"
+            f"docker pull sharp6292/{build.name}:{build.version} || true\n"
+            f"docker build --cache-from sharp6292/{build.name}:{build.version}"
+            f"-f Dockerfile -t sharp6292/{build}:{build.version} .\n"
+            f"docker push sharp6292/{build.name}:{build.version}\n"
+            "cd ..\n"
+        )
+        job_code += sh
+    with open(f'./builds/build-{num}.sh', 'w') as file:
+        file.write(job_code)
